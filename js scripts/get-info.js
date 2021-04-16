df078229071000bddaa714ba1eeac8867cc3bb27
Victor Alexandre Saraiva Pimentel <victor_alex2005@hotmail.com>
2021-04-16 20:43:20 -0300

    colocado tudo, falta apenas o css

---
+++ js scripts/get-info.js
@@ -0,0 +1,6 @@
+var now = new Date()
+var day = 1//now.getDay()
+var hour = 15//now.getHours()
+var min = 15//now.getMinutes()
+
+var agora = (hour * 100) + min
---
+++ js scripts/time-check.js
@@ -0,0 +1,26 @@
+
+
+switch (day) {
+    case 1:
+        time_check(seg)
+        break;
+
+    case 2:
+        time_check(ter)
+        break;
+
+    case 3:
+        time_check(quar)
+        break;
+
+    case 4:
+        time_check(quin)
+        break;
+
+    case 5:
+        time_check(sext)
+        break;
+
+    default:
+        break;
+}
